f6292845d0a1a6b2965fd09b4bae8440e9bd0c6a
Jason R. Wang <accounts@jrwang.ca>
2018-09-26 16:23:47 +0200

    Completed basic necessary steps for #8 and validated

--- raw_parser.R
+++ raw_parser.R
@@ -1,33 +1,60 @@
-tourn_raw = read.csv("tournament.csv",sep=",",stringsAsFactors = FALSE)
+raw_parser = function(file_name) {
 
-num_agents = max(unique(tourn_raw$id1))
+    tourn_raw = read.csv("validate.csv",sep=",",stringsAsFactors = FALSE)
+    num_agents = max(unique(tourn_raw$id1)) # Only works if IDs are sequential!
+    outcome_sequences = array(data=NA,dim=c(num_agents,num_agents,40)) # 40 is wasteful. See improvement suggestion below.
+    # typeof(outcome_sequences) ## just for testing
 
-outcome_sequences = array(data=NA,dim=c(num_agents,num_agents,50)) # 50 is wasteful. See improvement suggestion below.
-typeof(outcome_sequences) # just for testing
+    ## To get data about the number of games played between each pair
+    # seq_length_list = c()
 
-for (i_id1 in seq(num_agents)) { # The "attacking" agent
-    id1_index = which(tourn_raw$id1 %in% i_id1) # Find where agent #i_id1 plays
+    for (i_id1 in seq(num_agents)) { # The "attacking" agent
+        id1_index = which(tourn_raw$id1 %in% i_id1) # Find where agent #i_id1 plays
 
-    for (i_id2 in seq(num_agents)) { # The "defending" agent
-        # Agents cannot play themselves, so skip in this case.
-        if (i_id1 == i_id2) {
-            next
-        }
-        # cat("Agent 1: %i | Agent 2: %i",i_id1,i_id2)
-        id2_index = which(tourn_raw$id2 %in% i_id2) # Find where agent #i_id2 plays
+        for (i_id2 in seq(num_agents)) { # The "defending" agent
+            # Agents cannot play themselves, so skip in this case.
+            if (i_id1 == i_id2) {
+                next
+            }
 
-        ## The below can be an improvement, but doesn't quite work yet.
-        ## The "list" cannot replace the single element.
-        # The number of times i_id1 attacks i_id2 to help pre-allocate list
-        seq_list_len = length(which(id1_index %in% id2_index))
-        #outcome_sequences[i_id1,i_id2] = vector("list",seq_list_len) # pre-allocate
-        
-        for (i in seq(seq_list_len)){
-            # Insert function to determine the proper output sequence
-            outcome_sequences[i_id1,i_id2,i] = tourn_raw$bid1[id1_index[i]]
-            print(outcome_sequences[i_id1,i_id2,]) # This thing prints, but it doesn't keep to validate!
-            #outcome_sequences[i_id1][i_id2].append(tourn_raw$id1[i])
-            #list.append(outcome_sequences[[i_id1,i_id2]],tourn_raw$bid1[i])
+            id2_index = which(tourn_raw$id2 %in% i_id2) # Find where agent #i_id2 plays
+
+            ## The below can be an improvement, but doesn't quite work yet.
+            ## The "list" cannot replace the single element.
+            # The number of times i_id1 attacks i_id2 to help pre-allocate list
+            seq_list_len = length(which(id1_index %in% id2_index))
+            #outcome_sequences[i_id1,i_id2] = vector("list",seq_list_len) # pre-allocate
+
+            # To get data about the number of games played between each pair
+            #seq_length_list = c(seq_length_list,seq_list_len)
+
+            for (i in seq(seq_list_len)){
+                outcome_sequences[i_id1,i_id2,i] = axelrod_code(tourn_raw$bid1[id1_index[i]],tourn_raw$bid2[id1_index[i]])
+            }
         }
     }
+    return(outcome_sequences)
 }
+
+axelrod_code = function(bid1, bid2) {
+    # Determines the letter code associated with an interaction
+    if (bid1 == 'cooperate') {
+       if (bid1 == bid2) { # CC -> R
+            return('R')
+       } else { # CD -> S
+            return('S')
+       }
+    }  else if (bid1 == 'defect') {
+        if (bid1 == bid2) { # DD -> P
+            return('P')
+        } else { # DC -> T
+            return('T')
+        }
+    } else {
+       print("Error in cooperate-defect matching!")
+       break
+    }
+}
+
+## For testing
+# raw_parser("validate.csv")
