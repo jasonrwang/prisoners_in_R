ee17f617b919979dceb5c205430e6444f6c2d842
Jason R. Wang <accounts@jrwang.ca>
2018-10-03 16:32:28 +0200

    Final clean and run before submission

--- main.R
+++ main.R
@@ -7,13 +7,5 @@ data_frame = axelrod_encode(raw_parser('Example_Tournament/bid_register.csv'))
 
 ## Run parsed array through statistics
 axelrod_string = extract_statistics(data_frame)
-View(axelrod_string)
 
-## Modify main string with statistical results
-# Did Siemon complete this step already? Or is this still to be done?
-
-## Run agent
-
-out = raw_parser('Example_Tournament/bid_register.csv')
-
-# setwd("/Users/jrwang/Documents/Coding/prisoners_in_R/Example_Tournament")
+# The string above can be put into the agent manually now
